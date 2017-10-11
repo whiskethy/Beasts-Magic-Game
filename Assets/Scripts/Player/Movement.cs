bdfccf75b1d1fabaa89114ba49cc3cb4c9ee8512
Rodolfo Gomez <rgomezer@gmail.com>
2017-10-10 22:39:13 -0500

    Added simple player movement Added Knight Character Added Knight Temporary Texture Added more detail to Main World Merge branch 'master' of https://github.com/rvasqz86/SE-Studio

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] private float playerSpeed = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0.0f, -playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+}

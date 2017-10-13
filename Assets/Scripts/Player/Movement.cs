ac719ad891a2b1ae8bf73082d09b2505a61fd9c1
peilin82 <peilin@live.com>
2017-10-13 15:03:57 -0500

    I fixed the problem

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,41 +3,55 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Movement : MonoBehaviour
+
+
+public class Movement : NetworkBehaviour
 {
     [SerializeField] private float playerSpeed = 1.0f;
     [SerializeField] private bool useMobileControls = false;
 
     public AndroidJoystick joyStick;
-    private Animator animator;
-
+    CharacterController controller;
     // Use this for initialization
     void Start () {
+        controller = GetComponent<CharacterController>();
 
-        animator = GetComponent<Animator>();
+        if (isServer)
+        {
+            
+          //  Vector3 v3 = new Vector3(-3, 0, 2);
+           // Quaternion q = new Quaternion(0, 0, 0, 0);
+           // this.gameObject.transform.SetPositionAndRotation(v3,q);
+            //this.transform.position.Set(3, 3, 3);
+            transform.Translate(0.0f, 0.0f, 6f *Time.deltaTime);
+
+        }
+
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        
+    }
+    // Update is called once per frame
+    void Update () {
+        
+          useMobileControls = false;
         if (!useMobileControls)
         {
+            if (!isLocalPlayer)
+                return;
             if (Input.GetKey(KeyCode.W))
             {
                 transform.Translate(0.0f, 0.0f, playerSpeed * Time.deltaTime);
-                animator.SetBool("isWalkingForward", true);
-            }
-            else
-            {
-                animator.SetBool("isWalkingForward", false);
             }
 
             if (Input.GetKey(KeyCode.S))
             {
                 transform.Translate(0.0f, 0.0f, -playerSpeed * Time.deltaTime);
-                
             }
 
             if (Input.GetKey(KeyCode.D))
@@ -53,7 +67,7 @@ public class Movement : MonoBehaviour
         else
         {
             transform.Translate(joyStick.InputDirection * playerSpeed * Time.deltaTime);
-            
+            //controller.Move(joyStick.InputDirection * playerSpeed * Time.deltaTime);
         }
     }
 }

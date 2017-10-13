196b302d73e7302340e1a51feae2ccc9c5d90115
Rodolfo Gomez <rgomezer@gmail.com>
2017-10-12 21:50:58 -0500

    Improved joystick movement for mobile device

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,34 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
-
-    enum MovementState
-    {
-        LEFT,
-        RIGHT,
-        UP,
-        DOWN
-    }
-
+public class Movement : MonoBehaviour
+{
     [SerializeField] private float playerSpeed = 1.0f;
     [SerializeField] private bool useMobileControls = false;
 
-    private bool goLeft;
-    private bool goRight;
-    private bool goUp;
-    private bool goDown;
+    public AndroidJoystick joyStick;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-        goLeft = false;
-        goRight = false;
-        goUp = false;
-        goDown = false;
+       
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -57,56 +45,7 @@ public class Movement : MonoBehaviour {
         }
         else
         {
-            if(goUp)
-            {
-                transform.Translate(0.0f, 0.0f, playerSpeed * Time.deltaTime);
-            }
-
-            if(goDown)
-            {
-                transform.Translate(0.0f, 0.0f, -playerSpeed * Time.deltaTime);
-            }
-
-            if(goLeft)
-            {
-                transform.Translate(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
-            }
-
-            if(goRight)
-            {
-                transform.Translate(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
-            }
+            transform.Translate(joyStick.InputDirection * playerSpeed * Time.deltaTime);
         }
-    }
-
-    public void EnableButtonLeft()
-    {
-        goLeft = true;
-    }
-
-    public void EnableButtonRight()
-    {
-        goRight = true;
-    }
-
-    public void EnableButtonUp()
-    {
-        Debug.Log("Enable Button Up");
-
-        goUp = true;
-    }
-
-    public void EnableButtonDown()
-    {
-        goDown = true;
-    }
-
-    public void DisableButtonMovement()
-    {
-        Debug.Log("Disable Button use");
-        goLeft = false;
-        goRight = false;
-        goUp = false;
-        goDown = false;
     }
 }

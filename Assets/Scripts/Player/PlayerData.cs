c572e10043caa2992189003e579c22e7297200ed
Nick Gutierrez <Nick@loop-248-65.loop-wireless.depaul.edu>
2017-10-16 20:08:19 -0500

    Added Player Class Data
    May rework, but can pull from for UI and such

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour {
+    
+    public int currentHealth;
+    public int playerHealhPool;
+    public int movementSpeed;
+
+    public int strength;  //physical attacks
+    public int agility;   //blocking
+    public int wisdom;    //ranged attacks
+}

291acca055c43caaaef73704cb14a99bd1273693
Raul <rvasqz86@gmail.com>
2017-10-21 10:49:38 -0500

    added skeleton state machine

---
+++ Assets/Scripts/Player/State.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : StateMachineBehaviour {
+	protected bool enabled;
+	// Use this for initialization
+	void Start () {
+		bool enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void On () 
+	{
+		enabled = true;
+	}
+	public void Off ()
+	{
+		enabled = false;
+	}
+	public void Ack (){
+		Debug.Log ("ACK");
+	}
+}

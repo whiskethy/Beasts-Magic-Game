1f7961249707b58dfcf56891d94150ad4702e1c2
Raul <rvasqz86@gmail.com>
2017-10-21 11:29:53 -0500

    added health status text

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour {
 
@@ -11,4 +12,25 @@ public class PlayerData : MonoBehaviour {
     public int strength;  //physical attacks
     public int agility;   //blocking
     public int wisdom;    //ranged attacks
+
+	Text healthText;
+	private int deathRate;
+
+	public void start(){
+		movementSpeed = 1.0f;
+		currentHealth = 82;
+		playerHealhPool = 1;
+		deathRate = 1000;
+	}
+
+	public void Update(){
+		if (deathRate == 0) {
+			currentHealth -= 1;
+			deathRate = 1000;
+		}
+		deathRate -= 1;
+	
+		healthText = GameObject.Find("Text").GetComponent <Text> ();
+		healthText.text = "Health: "+ currentHealth.ToString();
+	}
 }

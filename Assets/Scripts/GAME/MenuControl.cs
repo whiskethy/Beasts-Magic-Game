eb83716c69b7760dd5f3cbe2de155e2b34742072
Raul <rvasqz86@gmail.com>
2017-10-31 01:02:55 -0500

    deleted extra script files and scenes, added feature for menu load levels

---
+++ Assets/Scripts/GAME/MenuControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl:MonoBehaviour {
+			
+	public void onClickPauseButton() {
+		Debug.Log ("Practice button clicked");
+		SceneManager.LoadScene("MainWorld");
+	}
+
+}

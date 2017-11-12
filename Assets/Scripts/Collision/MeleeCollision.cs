720948d1ebba7fe0558b342ab2c88b1ebe5652de
Rodolfo Gomez <rgomezer@gmail.com>
2017-11-12 02:08:53 -0600

    Added offline versions of play scripts to make them derive from MonoDevelop Began work on blocking

--- Assets/Scripts/Collision/MeleeCollision.cs
+++ Assets/Scripts/Collision/MeleeCollision.cs
@@ -6,8 +6,10 @@ public class MeleeCollision : MonoBehaviour {
 
     [SerializeField] private GameObject playerObj;
     [SerializeField] private HealthManager healthManager;
+    [SerializeField] private bool offlineMode;
 
     private PlayerAttack pAttack;
+    private PlayerAttackOffline pAttackO;
     private PlayerData pData;
 
 	// Use this for initialization
@@ -16,8 +18,16 @@ public class MeleeCollision : MonoBehaviour {
         healthManager = FindObjectOfType<HealthManager>();
         Debug.Assert(healthManager != null);
 
-        pAttack = playerObj.GetComponent<PlayerAttack>();
-        Debug.Assert(pAttack != null);
+        if (!offlineMode)
+        {
+            pAttack = playerObj.GetComponent<PlayerAttack>();
+            Debug.Assert(pAttack != null);
+        }
+        else
+        {
+            pAttackO = playerObj.GetComponent<PlayerAttackOffline>();
+            Debug.Assert(pAttack != null);
+        }
 
         pData = playerObj.GetComponent<PlayerData>();
         Debug.Assert(pData != null);
@@ -31,40 +41,81 @@ public class MeleeCollision : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player2" && pAttack.getAttack1)
+        if (!offlineMode)
         {
-            Debug.Log("Collided with: " +other.gameObject.name);
-            Debug.Log("Player Attack 1: " + pAttack.getAttack1);
+            if (other.gameObject.tag == "Player2" && pAttack.getAttack1)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 1: " + pAttack.getAttack1);
 
-            other.gameObject.GetComponent<PlayerAnimation>().lightHit();
-            healthManager.takeDamage(other.gameObject, 10);
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 10);
+            }
+
+            if (other.gameObject.tag == "Player2" && pAttack.getAttack2)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 2: " + pAttack.getAttack2);
+
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 20);
+            }
+
+            if (other.gameObject.tag == "Player1" && pAttack.getAttack1)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 1: " + pAttack.getAttack1);
+
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 10);
+            }
+
+            if (other.gameObject.tag == "Player1" && pAttack.getAttack2)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 2: " + pAttack.getAttack2);
+
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 20);
+            }
         }
-
-        if (other.gameObject.tag == "Player2" && pAttack.getAttack2)
+        else
         {
-            Debug.Log("Collided with: " + other.gameObject.name);
-            Debug.Log("Player Attack 2: " + pAttack.getAttack2);
+            if (other.gameObject.tag == "Player2" && pAttackO.getAttack1)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 1: " + pAttackO.getAttack1);
 
-            other.gameObject.GetComponent<PlayerAnimation>().lightHit();
-            healthManager.takeDamage(other.gameObject, 20);
-        }
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 10);
+            }
 
-        if (other.gameObject.tag == "Player1" && pAttack.getAttack1)
-        {
-            Debug.Log("Collided with: " + other.gameObject.name);
-            Debug.Log("Player Attack 1: " + pAttack.getAttack1);
+            if (other.gameObject.tag == "Player2" && pAttackO.getAttack2)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 2: " + pAttackO.getAttack2);
 
-            other.gameObject.GetComponent<PlayerAnimation>().lightHit();
-            healthManager.takeDamage(other.gameObject, 10);
-        }
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 20);
+            }
 
-        if (other.gameObject.tag == "Player1" && pAttack.getAttack2)
-        {
-            Debug.Log("Collided with: " + other.gameObject.name);
-            Debug.Log("Player Attack 2: " + pAttack.getAttack2);
+            if (other.gameObject.tag == "Player1" && pAttackO.getAttack1)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 1: " + pAttackO.getAttack1);
 
-            other.gameObject.GetComponent<PlayerAnimation>().lightHit();
-            healthManager.takeDamage(other.gameObject, 20);
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 10);
+            }
+
+            if (other.gameObject.tag == "Player1" && pAttackO.getAttack2)
+            {
+                Debug.Log("Collided with: " + other.gameObject.name);
+                Debug.Log("Player Attack 2: " + pAttackO.getAttack2);
+
+                other.gameObject.GetComponent<PlayerAnimation>().lightHit();
+                healthManager.takeDamage(other.gameObject, 20);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Player/Controlls/PlayerAttackOffline.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackOffline : MonoBehaviour {
+
+    private PlayerData pData;
+
+    //data
+    private bool attack1;
+    private bool attack2;
+    private bool canBlock;
+
+    private PlayerAnimation anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<PlayerAnimation>();
+        Debug.Assert(anim != null);
+        attack1 = false;
+        attack2 = false;
+        canBlock = false;
+
+        pData = GetComponent<PlayerData>();
+        Debug.Assert(pData != null);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !attack2)
+        {
+            Debug.Log("Attack 1 Start!");
+            attack1 = true;
+            StartCoroutine(DisableAttack1());
+        }
+
+        if (Input.GetMouseButtonDown(1) && !attack1)
+        {
+            attack2 = true;
+            Debug.Log("Attack 2 Start");
+            StartCoroutine(DisableAttack2());
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            canBlock = true;
+            Debug.Log("Blocking Attack");
+            anim.blocking();
+        }
+        //else
+        //{
+        //    Debug.Log("Blocking Attack Stop");
+        //    canBlock = false;
+        //}
+    }
+
+    private IEnumerator DisableAttack1()
+    {
+        anim.attackOne(attack1);
+        yield return new WaitForSeconds(pData.attack1CoolDown);
+        Debug.Log("Attack 1 Stop");
+        this.attack1 = false;
+        anim.attackOne(attack1);
+    }
+
+    private IEnumerator DisableAttack2()
+    {
+        anim.attackTwo(attack2);
+        yield return new WaitForSeconds(pData.attack2CoolDown);
+        this.attack2 = false;
+        Debug.Log("Attack 2 Stop");
+        anim.attackTwo(attack2);
+    }
+
+    public bool getAttack1
+    {
+        get { return attack1; }
+    }
+
+    public bool getAttack2
+    {
+        get { return attack2; }
+    }
+
+    public void Attack1()
+    {
+        attack1 = true;
+    }
+
+    public void Attack2()
+    {
+        attack2 = true;
+    }
+
+    public void Block()
+    {
+        canBlock = true;
+    }
+}

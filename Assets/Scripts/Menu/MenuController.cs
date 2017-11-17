57812c8f7af15cdc90dc3ddad7bf4075d7fb0889
Rodolfo Gomez <rgomezer@gmail.com>
2017-11-16 23:04:25 -0600

    Fused Network Lobby and Main Menu together.

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField] GameObject LobbyObject;
+    [SerializeField] GameObject MenuObject;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Assert(LobbyObject != null);
+        LobbyObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartNetworkLobby()
+    {
+        LobbyObject.SetActive(true);
+        MenuObject.SetActive(false);
+    }
+
+    public void StartPracticeScene()
+    {
+        LobbyObject.SetActive(false);
+        MenuObject.SetActive(false);
+
+        SceneManager.LoadScene(GameConstants.LEVELS.MainWorldPractice.ToString());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

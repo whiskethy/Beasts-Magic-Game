258edf38b1927b8960f444e7a9e8b9805690d15e
Rodolfo Gomez <rgomezer@gmail.com>
2018-03-05 11:49:16 -0600

    Fixed UI scaling and collision with projectiles in Practice Scene.

--- Assets/Scripts/Collision/ProjectileCollisionOffline.cs
+++ Assets/Scripts/Collision/ProjectileCollisionOffline.cs
@@ -43,5 +43,9 @@ public class ProjectileCollisionOffline : MonoBehaviour {
 
             Destroy(gameObject);
         }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
